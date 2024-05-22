f161c95bf2706b74c7d63c957cf8d943e183cbfa
Kittisak <Kittisaktaoma18@gmail.com>
2024-05-22 17:35:12 +0700

    unused file

--- codes/simulate-SFig3/plot_contoru.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-library(ggplot2)
-library(dplyr)
-df<- fread("plot_SFig3A.csv") %>% as.data.frame() %>% select(-V1)
-
-ggplot(df, aes(x=clb5_K_Theta,y=cdh1_K_Theta, z = Mean_Time)) + 
-  geom_contour()
-
-faithfuld
